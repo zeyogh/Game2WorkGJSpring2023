e9b0a02c52aac690b996a8480e5257aed6b22b08
egasner <egasner68@gmail.com>
2023-01-28 14:53:26 -0600

    stethoscope

--- Game2WorkGJSpring2023/Assets/Scripts/CursorController.cs
+++ Game2WorkGJSpring2023/Assets/Scripts/CursorController.cs
@@ -6,6 +6,8 @@ public class CursorController : MonoBehaviour
 {
 
     public Texture2D cursor;
+
+    public Texture2D cursor2;
     
 
     
@@ -16,10 +18,25 @@ public class CursorController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Confined;
     }
 
+    
+
     // Start is called before the first frame update
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+             ChangeCursor(cursor2);
+             Cursor.lockState = CursorLockMode.Confined;
+        }
+
+        if(Input.GetKeyUp(KeyCode.Mouse0)){
+            ChangeCursor(cursor);
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 
     private void ChangeCursor(Texture2D cursorType){
