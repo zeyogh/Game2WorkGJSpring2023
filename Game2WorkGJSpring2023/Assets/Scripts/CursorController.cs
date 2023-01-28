0d715a1d869a1dc99fd3199fb38367b68c6fb082
egasner <egasner68@gmail.com>
2023-01-28 12:36:04 -0600

    Custom Cursor Start(WIP)

---
+++ Game2WorkGJSpring2023/Assets/Scripts/CursorController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    public Texture2D cursor;
+    public Texture2D clickedCursor;
+
+    private void Awake(){
+        ChangeCursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void ChangeCursor(Texture2D cursorType){
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
